4ffe566ebdc2d1d8016e8bd9f5db5a5266fd10b8
DPIliev <mitii@mail.bg>
2016-06-04 12:13:13 +0300

    Create get_locations.php

---
+++ get_locations.php
@@ -0,0 +1,14 @@
+<?php
+  include 'config.php';
+  try {
+    $db = new PDO($dsn, $username, $password);
+    $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    $sth = $db->query("SELECT * FROM markers");
+    $markers = $sth->fetchAll();
+    echo json_encode( $markers );
+  } catch (Exception $e) {
+    echo $e->getMessage();
+  }
+  
+  
+  markers-name of the table with coordinate in my database

a9ffdc8c28fd8703231b2b420a12c47cee536420
DPIliev <mitii@mail.bg>
2016-06-04 12:16:27 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+  $server     = 'mysql8.000webhost.com';
+  $username   = 'a9699062_deset';
+  $password   = 'qwe517qwe512';
+  $database   = 'a9699062_devet';
+  $dsn        = "mysql:host=$server;dbname=$database";
+?>

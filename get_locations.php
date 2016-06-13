89d101f21f9fab15f08d9ce8707b6ffb7a35efb0
DPIliev <mitii@mail.bg>
2016-06-13 20:15:04 +0300

    Update get_locations.php

--- get_locations.php
+++ get_locations.php
@@ -1,14 +1,54 @@
 <?php
-  include 'config.php';
-  try {
-    $db = new PDO($dsn, $username, $password);
-    $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    $sth = $db->query("SELECT * FROM markers");
-    $markers = $sth->fetchAll();
-    echo json_encode( $markers );
-  } catch (Exception $e) {
-    echo $e->getMessage();
-  }
-  
-  
-  markers-name of the table with coordinate in my database
+require("phpsqlajax_dbinfo.php");
+
+function parseToXML($htmlStr)
+{
+$xmlStr=str_replace('<','&lt;',$htmlStr);
+$xmlStr=str_replace('>','&gt;',$xmlStr);
+$xmlStr=str_replace('"','&quot;',$xmlStr);
+$xmlStr=str_replace("'",'&#39;',$xmlStr);
+$xmlStr=str_replace("&",'&amp;',$xmlStr);
+return $xmlStr;
+}
+
+// Opens a connection to a MySQL server
+$connection=mysql_connect ('mysql8.000webhost.com', $username, $password);
+if (!$connection) {
+  die('Not connected : ' . mysql_error());
+}
+
+// Set the active MySQL database
+$db_selected = mysql_select_db($database, $connection);
+if (!$db_selected) {
+  die ('Can\'t use db : ' . mysql_error());
+}
+
+// Select all the rows in the markers table
+$query = "SELECT * FROM markers WHERE 1";
+$result = mysql_query($query);
+if (!$result) {
+  die('Invalid query: ' . mysql_error());
+}
+
+header("Content-type: text/xml");
+
+// Start XML file, echo parent node
+echo '<markers>';
+
+// Iterate through the rows, printing XML nodes for each
+while ($row = @mysql_fetch_assoc($result)){
+  // ADD TO XML DOCUMENT NODE
+  echo '<marker ';
+  echo 'name="' . parseToXML($row['name']) . '" ';
+  echo 'address="' . parseToXML($row['address']) . '" ';
+  echo 'lat="' . $row['lat'] . '" ';
+  echo 'lng="' . $row['lng'] . '" ';
+  echo 'type="' . $row['type'] . '" ';
+  echo '/>';
+}
+
+// End XML file
+echo '</markers>';
+
+?>
+
